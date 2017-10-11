c087958fa837123ca4488b47ed3902e31015bd85
rote66 <pontifex23872388@gmail.com>
2017-10-11 23:38:58 +0800

    Fix and update somethings from official
    Signed-off-by: rote66 <pontifex23872388@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Vendor Repo for the Meizu Meilan 2 (m2)
+Vendor Repo for the TCL_P620M
 =======================================
 
-Contains blobs (binaries) specific for m2.
+Contains blobs (binaries) specific for TCL P620M
 These blobs correspond to the proprietary-files.txt file in the device tree
--- prebuilt/libs/Android.mk
+++ prebuilt/libs/Android.mk
@@ -13,36 +13,6 @@ LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := MtkCamera
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := APPS
-LOCAL_CERTIFICATE   := platform
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/Camera/Camera.apk
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := libDocVfbEngineLib_m81
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/Camera/lib/arm/libDocVfbEngineLib_m81.so
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app/MtkCamera/lib/arm
-LOCAL_MULTILIB := 32
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := libpanorama
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/Camera/lib/arm/libpanorama.so
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app/MtkCamera/lib/arm
-LOCAL_MULTILIB := 32
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE        := libmeizucamera
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
